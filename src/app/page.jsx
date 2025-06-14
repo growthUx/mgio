7d313feed682bdefe332fbee883bd036331650cf
Victor <victor@ramayrat.com>
2025-06-14 11:04:23 -0700

    Update home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,6 +11,15 @@ import {
   SparklesIcon,
   SunIcon,
   UserGroupIcon,
+  LifebuoyIcon,
+  NewspaperIcon,
+  PhoneIcon,
+  ArrowPathIcon,
+  CloudArrowUpIcon,
+  Cog6ToothIcon,
+  FingerPrintIcon,
+  LockClosedIcon,
+  ServerIcon,
 } from '@heroicons/react/20/solid'
 import Footer from '@/components/Footer'
 
@@ -77,7 +86,56 @@ const benefits = [
   'A great work environment',
 ]
 
+const cards = [
+  {
+    name: 'GA4 and Google Tag Manager',
+    description: 'Track key user interactions and conversions across your website and app, giving you clear visibility into performance and user behavior.',
+    icon: PhoneIcon,
+  },
+  {
+    name: 'Segment CDP',
+    description: 'Collect, unify, and send customer data to all your tools—ensuring consistent insights across your marketing, analytics, and CRM stack.',
+    icon: LifebuoyIcon,
+  },
+  {
+    name: 'Amplitude / PostHog',
+    description: 'For deeper product insights, analyze feature usage, user flows, and retention so you can optimize for engagement and growth.',
+    icon: NewspaperIcon,
+  },
+]
 
+const features = [
+  {
+    name: 'Push to deploy.',
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.',
+    icon: CloudArrowUpIcon,
+  },
+  {
+    name: 'SSL certificates.',
+    description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
+    icon: LockClosedIcon,
+  },
+  {
+    name: 'Simple queues.',
+    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus.',
+    icon: ArrowPathIcon,
+  },
+  {
+    name: 'Advanced security.',
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.',
+    icon: FingerPrintIcon,
+  },
+  {
+    name: 'Powerful API.',
+    description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
+    icon: Cog6ToothIcon,
+  },
+  {
+    name: 'Database backups.',
+    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. ',
+    icon: ServerIcon,
+  },
+]
 export default function HomePage() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
@@ -185,13 +243,32 @@ export default function HomePage() {
         {/* Header section */}
         <div className="px-6 pt-14 lg:px-8">
           <div className="mx-auto max-w-3xl sm:max-w-5xl pt-24 text-center sm:pt-40">
-            <h1 className="text-5xl font-semibold tracking-tight text-[#F5F5F5] sm:text-7xl">Make your website a predictable growth channel </h1>
-            <p className="mt-8 text-lg font-medium text-pretty text-[#F5F5F5] sm:text-xl/8">
-            We will help you implement a membership-based platform for your brand to grow. Then, leverage AI agents to power your GTM and sales workflow automation so you can scale. 
-            </p>
+            <h1 className="text-5xl font-semibold tracking-tight text-[#F5F5F5] sm:text-7xl">Establish trust and authority to attract more customers</h1>
+            <p className="mt-8 text-lg max-w-3xl mx-auto font-medium text-pretty text-[#F5F5F5] sm:text-xl/8">It all starts with a well-designed brand to tell your story, delivered on a scalable subscription-based platform featuring your business&apos; value proposition. Then scale through AI-powered workflow automation with analytics and reporting. <br /><br/><strong>We&apos;re here to help!</strong></p>
+          </div>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <a
+              href="mailto:victor@marketgrowth.io"
+              className="rounded-md bg-green-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-green-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+            >
+              Get a free consult
+            </a>
           </div>
         </div>
-
+        
+        {/* Community screenshot section */}
+        <div className="mt-16 max-w-7xl mx-auto flow-root sm:mt-24 px-4">
+          <div className="-m-2 rounded-xl bg-white/5 p-2 ring-1 ring-white/10 ring-inset lg:-m-4 lg:rounded-2xl lg:p-4">
+            <img
+              alt="MarketGrowth Community"
+              src="/images/marketgrowth-ss.png"
+              width={2432}
+              height={1442}
+              className="rounded-md shadow-2xl ring-1 ring-gray-900/10 transition-opacity duration-300 ease-in-out opacity-30 hover:opacity-100"
+            />
+          </div>
+          
+        </div>
         {/* Content section */}
         <div className="mx-auto mt-20 max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
@@ -201,21 +278,75 @@ export default function HomePage() {
                   <h3 className="text-xl font-semibold mb-4">Delightful digital experience as a revenue driver</h3>
                   <p>Your website should be more than a storefront. It has the potential to become your most consistent and measurable growth channel. As a digital experience agency, we help you transform your site into a performance-driven platform that attracts, engages, and converts visitors into loyal customers and community members.
                   </p>
-                  </div>
-                <div className="mb-8">
+                </div>
+              </div>
+              <div>
+                  <div className="mb-8">
                   <h3 className="text-xl font-semibold mb-4">A membership-based platform that drives engagement</h3>
                   <p>
                   We help you implement paid or free membership-based platforms that align with your brand and business goals. Whether you are a service provider, creator, or coach, we will help you offer exclusive content, programs, and experiences that keep your audience engaged and invested. You choose the platform &mdash; whether it is open source or use vendors like Gradual.
                   </p>
                 </div>
               </div>
+            </div>
+          </div>
+        </div>
+        <div className="relative py-24 sm:py-32">
+        <div className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl">
+            <div
+              style={{
+                clipPath:
+                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              }}
+              className="aspect-1097/845 w-274.25 bg-linear-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+            />
+          </div>
+          <div className="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu">
+            <div
+              style={{
+                clipPath:
+                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              }}
+              className="aspect-1097/845 w-274.25 bg-linear-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+            />
+          </div>
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <div className="mx-auto max-w-4xl sm:text-center">
+              <h2 className="text-base/7 font-semibold text-[#f5f5f5]">Power your business with AI agents</h2>
+              <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-[#f5f5f5] sm:text-5xl sm:text-balance">
+              Accelerate your go-to-market and sales motion with automated workflows
+              </p>
+              <p className="mt-6 max-w-2xl  mx-auto text-lg/8 text-gray-300">
+                We can set up your workflow automation tools like n8n within your own servers or use cloud services.
+              </p>
+            </div>
+          </div>
+          <div className="pt-16">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+              <div className="-m-2 rounded-xl bg-white/5 p-2 ring-1 ring-white/10 ring-inset lg:-m-4 lg:rounded-2xl lg:p-4">
+                <img
+                  alt="MarketGrowth N8N AI Workflow Automation"
+                  src="/images/n8n-ai-workflow.png"
+                  width={2432}
+                  height={1442}
+                  className="rounded-xl shadow-2xl ring-1 ring-gray-900/10 transition-opacity duration-300 ease-in-out opacity-50 hover:opacity-100"
+                />
+              </div>
+            </div>
+          </div>
+          {/* Content section */}
+        <div className="mx-auto mt-20 max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
+            <div className="grid max-w-xl grid-cols-1 gap-14 text-base/7 text-[#f5f5f5] lg:max-w-none lg:grid-cols-2">
               <div>
-                  <div className="mb-8">
+                <div className="mb-8">
                   <h3 className="text-xl font-semibold mb-4">Automate GTM and sales workflows with AI agents</h3>
                   <p>
                     After launching your platform, we implement AI-powered agents that automate key parts of your go-to-market and sales operations. From qualifying leads to delivering personalized follow-ups, these automations free up your time and increase your ability to scale efficiently. The result is a leaner, smarter system that works for you around the clock.
                   </p>
                 </div>
+              </div>
+              <div>
                 <div className="mb-8">
                   <h3 className="text-xl font-semibold mb-4">Grow with a system designed for long-term success</h3>
                   <p>
@@ -224,6 +355,9 @@ export default function HomePage() {
                 </div>
               </div>
             </div>
+        </div>
+        
+            
             {/* <dl className="mt-16 grid grid-cols-1 gap-x-8 gap-y-12 sm:mt-20 sm:grid-cols-2 sm:gap-y-16 lg:mt-28 lg:grid-cols-4">
               {stats.map((stat, statIdx) => (
                 <div key={statIdx} className="flex flex-col-reverse gap-y-3 border-l border-white/20 pl-6">
@@ -234,15 +368,41 @@ export default function HomePage() {
             </dl> */}
           </div>
         </div>
-
-        {/* Image section */}
-        <div className="mt-32 sm:mt-40 xl:mx-auto xl:max-w-7xl xl:px-8">
+        {/* Analytics */}
+        <div className="relative isolate overflow-hidden bg-[#1D3c34] py-24 sm:py-32">
+          <img
+            alt=""
+            src="https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2894&q=80"
+            className="absolute inset-0 -z-10 size-full object-cover object-right md:object-center transition-opacity duration-300 ease-in-out opacity-20 mix-blend-soft-light hover:opacity-40"
+          />
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <div className="mx-auto max-w-2xl lg:mx-0">
+              <h3 className="text-xl font-semibold tracking-tight text-white sm:text-3xl">Measure. Improve. Market growth.</h3>
+              <p className="mt-8 text-lg font-medium text-pretty text-[#f5f5f5] sm:text-xl/8">
+              We integrate analytics that track user behavior, engagement, and conversion across your platform. These insights help you understand what&apos;s working, where users drop off, and how to optimize for better performance. With clear metrics tied to your goals, you can measure ROI, refine your strategy, and make confident, data-driven decisions.
+              </p>
+            </div>
+            <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-6 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-8">
+              {cards.map((card) => (
+                <div key={card.name} className="flex gap-x-4 rounded-xl bg-white/5 p-6 ring-1 ring-white/10 ring-inset">
+                  {/* <card.icon aria-hidden="true" className="h-7 w-5 flex-none text-indigo-400" /> */}
+                  <div className="text-base/7">
+                    <h3 className="font-semibold text-white">{card.name}</h3>
+                    <p className="mt-2 text-gray-300">{card.description}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+         {/* Image section */}
+         {/* <div className="mt-32 sm:mt-40 xl:mx-auto xl:max-w-7xl xl:px-8">
           <img
             alt=""
             src="https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2894&q=80"
             className="aspect-9/4 w-full object-cover xl:rounded-3xl"
           />
-        </div>
+        </div> */}
 
         {/* Values section */}
         {/* <div className="mx-auto mt-32 max-w-7xl px-6 sm:mt-40 lg:px-8">
