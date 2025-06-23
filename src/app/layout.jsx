d91b4cfa6b9babfb39d077c7cee600ab78aed35b
Victor <victor@ramayrat.com>
2025-06-23 01:48:15 -0700

    Update with GTM

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,6 +3,7 @@ import localFont from 'next/font/local'
 import clsx from 'clsx'
 
 import { Providers } from '@/app/providers'
+import GoogleTagManager from '@/components/GoogleTagManager'
 
 import '@/styles/tailwind.css'
 
@@ -25,6 +26,7 @@ const funnelDisplay = localFont({
   variable: '--font-funnel-display',
   weight: '300 800',
 })
+
 export const metadata = {
   title: 'Digital Experience and AI Automation Agency',
   description:
@@ -50,17 +52,8 @@ export default function RootLayout({ children }) {
       className={clsx('h-full antialiased', inter.variable, funnelDisplay.variable, monaSans.variable)}
       suppressHydrationWarning
     >
-      <head>
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-TNR79XR');</script>
-        <link rel="icon" type="image/png" href="/favicon.png" />
-      </head>
       <body className="flex min-h-full flex-col bg-[#1D3c34]">
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TNR79XR"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <GoogleTagManager />
         <Providers>{children}</Providers>
       </body>
     </html>
