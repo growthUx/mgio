105c01df6ef6256c4818085aa9ffa3e774b3560c
Victor <victor@ramayrat.com>
2025-06-27 12:45:49 -0700

    Update HomePage.jsx

--- src/app/HomePage.jsx
+++ src/app/HomePage.jsx
@@ -115,7 +115,7 @@ export default function HomePage() {
                 </a>
               </div>
               <h1 className="font-funnel-display text-4xl font-semibold tracking-tight text-[#F5F5F5] sm:text-6xl">We are an on-demand technical team to help with your go-to-market</h1>
-              <p className="mt-8 text-md max-w-2xl mx-auto text-pretty text-[#F5F5F5] sm:text-lg/8 sm:mx-0 sm:pr-6">Lean on us to help with the technical execution of your business&apos; digital engagement, either with inbound-, community- or agent-led GTM motion.</p>
+              <p className="mt-8 text-md max-w-2xl mx-auto text-pretty text-[#F5F5F5] sm:text-lg/8 sm:mx-0 sm:pr-6">Lean on us to help with the orchestration or management of your GTM technology &mdash;  from digital experience and AI-powered automation to analytics and reporting. We&apos;re here to help!</p>
               <div className="mt-10 flex items-center gap-x-6">
                 <a
                   href="mailto:victor@marketgrowth.io"
